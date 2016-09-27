02451ba67e493f34e8bb50d17e347292b4c243cc
maple3142 <kirby741852963@gmail.com>
2016-09-27 22:49:39 +0800

    update

--- js/login.js
+++ js/login.js
@@ -1,6 +1,7 @@
 function login(){
 	var pwd=document.getElementById("pwd").value;
-	if(pwd=="惠比壽麝香葡萄"){
+	if(pwd=="惠比壽麝香葡萄")
 		document.getElementById("content").removeAttribute("hidden");
-	}
+	else
+		document.getElementById("content").setAttribute("hidden");
 }
