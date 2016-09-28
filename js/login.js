e3983491d93c624a2a940849111aab55a49d9320
maple3142 <kirby741852963@gmail.com>
2016-09-28 19:03:46 +0800

    bugfix

--- js/login.js
+++ js/login.js
@@ -2,15 +2,15 @@ function login(){
 	var ans="惠比壽麝香葡萄";
 	var pwd=$("#pwd").val();
 	var logined=false;
-	if(pwd==ans && !logined){
-		document.getElementById("content").removeAttribute("hidden");
-		Materialize.toast('登入成功',3000,'rounded');
-		logined=true;
-	}
-	
-	if(pwd==ans && logined){
-		Materialize.toast('已登入',3000,'rounded');
-	}
+	if(pwd==ans && !logined)
+		if(logined){
+			Materialize.toast('已登入',3000,'rounded');
+		}
+		else{
+			document.getElementById("content").removeAttribute("hidden");
+			Materialize.toast('登入成功',3000,'rounded');
+			logined=true;
+		}
 	
 	if(pwd!=ans){
 		document.getElementById("content").setAttribute("hidden");
