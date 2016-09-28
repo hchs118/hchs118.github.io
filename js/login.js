561257c05944d7ffaab82e27995013333f28f6fc
maple3142 <kirby741852963@gmail.com>
2016-09-28 18:57:15 +0800

    test

--- js/login.js
+++ js/login.js
@@ -4,13 +4,13 @@ function login(){
 	var logined=false;
 	if(pwd==ans && !logined){
 		document.getElementById("content").removeAttribute("hidden");
-		//Materialize.toast('登入成功',3000,'rounded');
+		Materialize.toast('登入成功',3000,'rounded');
 	}
 	else if(pwd==ans && logined){
-		//Materialize.toast('已登入',3000,'rounded');
+		Materialize.toast('已登入',3000,'rounded');
 	}
 	else if(pwd!=ans){
 		document.getElementById("content").setAttribute("hidden");
-		//Materialize.toast('登入失敗',3000,'rounded');
+		Materialize.toast('登入失敗',3000,'rounded');
 	}
 }
