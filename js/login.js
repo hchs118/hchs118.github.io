494782162de116d9c7732ae82226a1ad24869a8f
maple3142 <kirby741852963@gmail.com>
2016-09-28 20:01:17 +0800

    update

--- js/login.js
+++ js/login.js
@@ -2,7 +2,13 @@ function login(){
 	var ans="惠比壽麝香葡萄";
 	var pwd=$("#pwd").val();
 	var logined=false;
-	if(pwd==ans && !logined)
+	if(pwd!=ans){
+		document.getElementById("content").setAttribute("hidden");
+		Materialize.toast('登入失敗',3000,'rounded');
+		logined=!logined;
+	}
+	
+	if(pwd==ans)
 		if(logined){
 			Materialize.toast('已登入',3000,'rounded');
 		}
@@ -11,9 +17,5 @@ function login(){
 			Materialize.toast('登入成功',3000,'rounded');
 			logined=!logined;
 		}
-	else{
-		document.getElementById("content").setAttribute("hidden");
-		Materialize.toast('登入失敗',3000,'rounded');
-		logined=!logined;
-	}
+	
 }
