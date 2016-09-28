df94882554b8948c657700d29b91c7f22b989a51
maple3142 <kirby741852963@gmail.com>
2016-09-28 18:55:06 +0800

    update

--- js/login.js
+++ js/login.js
@@ -1,14 +1,15 @@
 function login(){
+	var ans="惠比壽麝香葡萄";
 	var pwd=$("#pwd").val();
-	boolean logined=false;
-	if(pwd=="惠比壽麝香葡萄" && !logined){
+	var logined=false;
+	if(pwd==ans && !logined){
 		document.getElementById("content").removeAttribute("hidden");
 		//Materialize.toast('登入成功',3000,'rounded');
 	}
-	else if(pwd=="惠比壽麝香葡萄" && logined){
+	else if(pwd==ans && logined){
 		//Materialize.toast('已登入',3000,'rounded');
 	}
-	else if(pwd!="惠比壽麝香葡萄"){
+	else if(pwd!=ans){
 		document.getElementById("content").setAttribute("hidden");
 		//Materialize.toast('登入失敗',3000,'rounded');
 	}
