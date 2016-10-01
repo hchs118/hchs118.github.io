17e8f00bdfcc82305fea4417cdb57ea499cb2cf6
maple3142 <kirby741852963@gmail.com>
2016-10-01 18:21:09 +0800

    remove mdwiki

--- board/board.php
+++ board/board.php
@@ -6,7 +6,19 @@
 		<link type="text/css" rel="stylesheet" href="../css/materialize.min.css"  media="screen,projection"/>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 	</head>
-	<body style="padding-top:15px;padding-left:10px;">
+	<body>
+		<nav>
+			<div class="nav-wrapper">
+			<a href="javascript: window.location.reload();" class="brand-logo center">留言板</a>
+		<ul id="nav-mobile" class="left hide-on-med-and-down">
+			<li><a href="../index.html">首頁</a></li>
+			<li><a href="../content/login.html">班級登入系統</a></li>
+			<li class="active"><a href="../board/board.php">留言板</a></li>
+			<li><a href="../content/test.html">測試用</a></li>
+		</ul>
+			</div>
+		</nav>
+		<div style="padding-top:15px;padding-left:10px;">
 		<form action="write.php" method="POST">
 			<input required name="name" placeholder="名字" style="width:50%;"></input><br>
 			<input required name="msg" placeholder="訊息" style="width:50%;"></input><br>
@@ -40,7 +52,6 @@
 				</div>
 			</div>
 		</div>
-		<button class="btn" onclick="window.location.href=&quot;../&quot;;">回首頁</button>
-		<button class="btn" onclick="window.location.reload();">重新整理</button>
+		</div>
 	</body>
 </html>
--- navigation.md
+++
@@ -1,9 +0,0 @@
-<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-<script type="text/javascript" src="js/materialize.min.js"></script>
-
-# 新竹中學118
-
-[留言板](board/board.php)
-[班級登入系統](content/login.html)
-[測試(可能會lag)](content/test.html)
